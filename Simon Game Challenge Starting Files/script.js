77cb12f712eb3b427200162525c02061c569a360
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-24 13:43:20 +0300

    added jquery flash animation

--- Simon Game Challenge Starting Files/script.js
+++ Simon Game Challenge Starting Files/script.js
@@ -3,9 +3,13 @@ let gamePattern=[];
 
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColour= buttonColours(randomNumber);
+    let randomChosenColour= buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100);
+
 };
+
+
 
 nextSequence();
